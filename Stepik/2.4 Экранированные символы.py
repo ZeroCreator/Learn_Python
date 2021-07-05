c58690099c0ae461a3650b9836edac8b6d2892a9
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2021-07-05 17:13:41 +0300

    Lesson 2.5

--- Stepik/2.4 Экранированные символы.py
+++ Stepik/2.4 Экранированные символы.py
@@ -1,5 +1,37 @@
 # ЭКРАНИРОВАННЫЕ (СЛУЖЕБНЫЕ) СИМВОЛЫ.
 
+s = '''hello
+world
+hy
+bye'''
+print(s)
+print(len(s))
+
+#
+print('1. "\\n" - перенос строки')
+f = 'hello\nword\nhi\nbye'
+print(f)
+print(len(f))
+
+a = 'abc\ndef'
+print(a)
+print(len(a))
+
+#
+print('2. "\\t" - табуляция')
+print('hello\nword\nhi\nbye')
+
+#
+print('3. "r" - перед началом строки - убрать служебные символы в строке:')
+print('hello\nword\nhi\nbye/thello\nword\nhi\nbye')
+print(r'hello\nword\nhi\nbye/thello\nword\nhi\nbye')
+
+a = 'abc\nre\tgfd\ngfd'
+print(a)
+print(len(a))
+b = r'abc\nre\tgfd\ngfd'
+print(len(b))
+print(b)
 #
 print('Tasks')
 #
