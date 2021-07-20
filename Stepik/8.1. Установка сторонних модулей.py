fac2824853764f976e65279165da0a3b7cacce31
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2021-07-20 13:46:11 +0300

    Lesson 6.1.1

---
+++ Stepik/8.1. Установка сторонних модулей.py
@@ -0,0 +1,9 @@
+# Установка сторонних модулей в Python.
+# 1. Способ - с помощью cmd и pip.
+# Посмотреть установленное - pip freeze
+# Установить модуль - pip install *** ==xx.x (версия)
+# 2. Способ - пакетная установка.
+# pip freeze > requirements.txt - имена интересующих модулей на сайте pypi.org
+# pip install -r requirements.txt
+# С помощь pycharm.
+
