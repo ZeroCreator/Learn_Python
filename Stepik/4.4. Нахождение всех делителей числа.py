2b700b9946fb6ad21ff93b4340b664857006dcee
ZeroCreator <ZeroCreator@yahoo.com>
2021-07-21 13:38:44 +0300

    Lesson 4.4

--- Stepik/4.4. Нахождение всех делителей числа.py
+++ Stepik/4.4. Нахождение всех делителей числа.py
@@ -1,5 +1,5 @@
 # НАХОЖДЕНИЕ ВСЕХ ДЕЛИТЕЛЕЙ ЧИСЛА.
-print('Динейные вычисления:')
+print('линейные вычисления:')
 n = int(input())
 i = 1
 while i <= n:
@@ -7,6 +7,7 @@ while i <= n:
         print(i, end=' ')
     i += 1
 
+# Ускорение программы в 2 раза:
 n = int(input())
 i = 1
 while i <= n // 2:
@@ -21,7 +22,7 @@ n = int(input())
 i = 1
 while i <= n**0.5:
     if n % i == 0:
-        print(i, n // i)        # Дублируется среднее значение
+        print(i, n // i)        # Дублируется значение серединное
     i += 1
 
 n = int(input())
@@ -63,7 +64,7 @@ print(a)
 
 # Программа получает на вход натуральное число N.
 # Нужно найти сумму его делителей.
-a=int(input())
+a = int(input())
 c = 0
 i = 0
 while c != a:
@@ -101,3 +102,138 @@ print(sum(a))
 
 n = int(input())
 print(sum(i for i in range(1, n + 1) if n % i == 0))
+
+# Дано натуральное число N. Определить, является ли оно простым. Натуральное число N называется простым, если у него
+# есть только два делителя: единица и само число N.
+# В качестве ответа выведите "Yes", если число простое,  "No" - в противном случае.
+x = int(input())
+a = x
+while a > 2:
+    a -= 1
+    if x % a == 0:
+        print('No')
+        break
+else:
+    if x == 1:
+        print('No')
+    else:
+        print('Yes')
+
+#
+a = int(input())
+i = 2
+while a % i != 0 and i < a:
+    i += 1
+print('Yes' if i == a else 'No')
+
+#
+def dig(x):
+    i = 2
+    while x % i != 0 and i < x:
+        i += 1
+    return i == x
+
+print(['No', 'Yes'][dig(int(input()))])
+
+#
+def dig(x):
+    i = 2
+    while x % i != 0 and i < x:
+        i += 1
+    return i == x
+
+print(['No', 'Yes'][dig(int(input()))])
+
+#
+n = int(input())
+i = 1
+count = 0
+while i < n ** 0.5:
+    if n % i == 0:
+        count += 2
+    i += 1
+print('Yes' if count == 2 else 'No')
+
+#
+n = int(input())
+if n==1:
+  print('No')
+else:
+  print('YNeos'[any(1 for i in range(2,n) if n%i==0)::2])
+
+#
+a = int(input())
+b = 2
+cnt = 0
+while b*b <= a:
+    if a % b == 0:
+        cnt += 1
+    b += 1
+if cnt == 0 and a != 1:
+    print("Yes")
+else:
+    print('No')
+
+#
+n, f = int(input()), False
+for _ in range(2, int(n**.5) + 1):
+    if not n % _:
+        f = True
+        break
+print(['Yes', 'No'][n == 1 or f])
+
+#
+n = int(input())
+f = True
+if n == 1 or n != 2 and not n % 2:
+    f = False
+else:
+    for i in range(3, int(n ** .5 + 1), 2):
+        if not n % i:
+            f = False
+            break
+print("YNeos"[1 - f::2])
+
+#
+n = int(input())
+a = 2
+while n % a != 0 and a < n:
+    a += 1
+if a == n:
+    print("Yes")
+else:
+    print("No")
+
+#
+n = int(input())
+print(('No', 'Yes')[n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))])
+
+#
+n = int(input())
+i = 1
+dividers = []  # список делителей числа n
+
+while i ** 2 <= n:  # находим все делители числа n
+    if n % i == 0:
+        dividers.append(i)  # накапливаем делители n в списоке делителей
+        if i != n // i:
+            dividers.append(n // i)  # отсееваем повторы
+    i += 1
+
+# простые числа делятся без остатка только на себя и еденице, следовательно
+# длина списка делителей простого числа равна 2
+if len(dividers) == 2:
+    print('Yes')  # число простое
+else:
+    print('No')
+
+#
+a = int(input())
+s = []
+for i in range(1,a + 1):
+    if a % i == 0:
+        s.append(i)
+if len(s) == 2:
+    print('Yes')
+else:
+    print('No')
--- Stepik/6.1.1. Как отсортировать словарь.py
+++ Stepik/6.1.1. Как отсортировать словарь.py
@@ -1,1 +1,123 @@
 # Как отсортировать словарь по ключам и значениям.
+# Вывод по ключу:
+heroes = {
+    'Spider-Man': 80,
+    'Batman': 65,
+    'Superman': 85,
+    'Wonder Woman': 70,
+    'Flash': 70,
+    'Iron Man': 65,
+    'Thor': 90,
+    'Aquaman': 65,
+    'Captain America': 65,
+    'Hulk': 87
+}
+#for i in heroes:
+#    print(i)
+
+# Как обратиться к значению (получаем пары):
+for i in heroes:
+    print(i, heroes[i])
+
+# Сортировка ключей в алфавитном порядке:
+for i in sorted(heroes):
+    print(i, heroes[i])
+
+# Если нужно обойти значения в упорядоченном виде - метод .values():
+for i in sorted(heroes.values()):
+    print(i)
+
+# Отсортировать значение через ключ - метод .items(), key = lambda para:
+for i in sorted(heroes.items(), key=lambda para: para[1]):                # Возвращает кортеж
+    print(i)
+
+# Сортировка и по ключу, и по значению:
+for i in sorted(heroes.items(), key=lambda para: (para[1], para[0])):                # Возвращает кортеж
+    print(i)
+
+#
+print('Tasks')
+# Представьте, у нас есть список товаров и их стоимость, но мы хотим взглянуть на него в отсортированном виде.
+# Вверху хотим видеть самые дорогие товары, внизу самые дешевые
+# Программа будет принимать строки, в которых сперва указывается название товара, а затем через двоеточие с пробелом
+# его цена - целое положительное число.
+# Строка "конец" означает списка товаров и соответственно окончание ввода
+# Все товары имеют уникальные названия, цены не дублируются.
+# Ваша задача вывести список товаров по уменьшению цены
+s = {}
+while True:
+    x = list(input().split(':'))
+    if x[0] == 'конец':
+        break
+    else:
+        k, v = x[0], x[1]
+    s[k] = int(v)
+
+for i in sorted(s.items(), key=lambda para: para[1], reverse=True):
+        print(i[0])
+
+#
+dict={}
+while True:
+    a = input()
+	if a == 'конец':
+		break
+	else:
+		temp_lst = a.split(': ')
+		dict[temp_lst[0]] = int(temp_lst[1])
+
+for key, value in sorted(dict.items(), key=lambda x: x[1], reverse=True):
+	print(key)
+
+#
+d = sorted([s.split(': ') for s in iter(input, 'конец')], key=lambda x: -int(x[1]))
+for v in d:
+    print(v[0])
+
+#
+lis={}
+while True:
+    all=input()
+    if all=='конец':
+        break
+    else:
+        a,b = map(str,all.split(':'))
+        lis[a]=int(b)
+for i,j in sorted(lis.items(),key=lambda para:para[1],reverse=True):
+    print(i)
+
+#
+d = {}
+while True:
+    inn = input()
+    if inn == 'конец':
+        break
+    k, price = inn.split(': ')
+    d[k] = int(price)
+[print(f'{k}') for k in sorted(d, key=d.get, reverse=True)]
+
+#
+product_list = {}
+
+while True:
+    p = input()
+    if p == 'конец':
+        break
+    prod, price = p.split(': ')
+    product_list[prod] = int(price)
+
+for key, val in sorted(product_list.items(), key=lambda para: para[1], reverse=True):
+    print(key)
+
+#
+enter, ls = input().replace(':', ''), []
+while enter != 'конец':
+    ls.append(enter.split())
+    enter = input().replace(':', '')
+[print(*i[:-1]) for i in sorted(ls, key=lambda x: -int(x[-1]))]
+
+#
+l = list(iter(input, 'конец'))
+for i in sorted(l, key=lambda cena: -int(cena.split()[-1])):
+    print(i[:(i.index(':'))])
+
