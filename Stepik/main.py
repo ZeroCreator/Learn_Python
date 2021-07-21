d95f16b27d95ceec94cd6221a8e635182dd2a955
ZeroCreator <ZeroCreator@yahoo.com>
2021-07-21 11:57:20 +0300

    Lesson 8.4 __init__.py main.py

--- Stepik/main.py
+++ Stepik/main.py
@@ -8,13 +8,22 @@
 #print(test_package.file2.d)
 
 # 2. Импорт самого модуля:
-from test_package import file1
-from test_package import file2
+#from test_package import file1
+#from test_package import file2
 
-print(file1.c)
-print(file2.d)
+#print(file1.c)
+#print(file2.d)
 
 # Импорт конкретных имен из модуля:
-from test_package.file1 import b
-from test_package.file2 import h
-print(b, h)
+#from test_package.file1 import b
+#from test_package.file2 import h
+#print(b, h)
+
+# Для того, чтобы были доступны все имена из
+# # импортируемого модуля из всех файлов -
+# # используем файл __init__ (см. файл __init__):
+import test_package
+
+print(test_package.h)
+
+#print(test_package.file1.c)
