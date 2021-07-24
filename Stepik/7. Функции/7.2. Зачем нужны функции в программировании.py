9292c8f21f37e933ec2e66f092fbf98e851c7f7c
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2021-07-24 15:15:27 +0300

    Lesson 7.2

--- Stepik/7. Функции/7.2. Зачем нужны функции в программировании.py
+++ Stepik/7. Функции/7.2. Зачем нужны функции в программировании.py
@@ -2,18 +2,32 @@
 # Помогают избавиться от избыточности кода.
 import turtle
 
+def move():
+    turtle.forward(100)
+    turtle.left(90)
+
 def draw_squere():
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
+    for i in range(4):
+        move()
 
 turtle.speed(1)
 
 draw_squere()
 turtle.goto(150, 150)
 draw_squere()
+
+# Позволяют декомпозировать решение проблемы (большую задачу разбить на маленькие функции и выстроить взаимосвязь между ними).
+
+def move(a):
+    turtle.forward(a)
+    turtle.left(90)
+
+def draw_squere(a):
+    for i in range(4):
+        move(a)
+
+turtle.speed(1)
+
+draw_squere(60)
+turtle.goto(150, 150)
+draw_squere(120)
