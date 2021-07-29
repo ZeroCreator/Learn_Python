55e215a22f78d77d374c21f473c3496c4160d4eb
ZeroCreator <ZeroCreator@yahoo.com>
2021-07-29 14:04:54 +0300

    Lesson 5.10

--- Stepik/5. Итерируемые объекты и цикл for/5.10. Вложенные генераторы списков.py
+++ Stepik/5. Итерируемые объекты и цикл for/5.10. Вложенные генераторы списков.py
@@ -26,3 +26,23 @@ b = [elem[0][0] for elem in a if elem[1] > 2000]
 print(b)
 
 # Обработка словарей:
+a = {
+    'Sidorov': {'age': 1995, 'hobby': 'soccer', 'car': 'BMW'},
+    'Lukov': {'age': 2002, 'hobby': 'basketball', 'car': 'Opel'},
+    'Petrov': {'age': 1991, 'hobby': 'chess', 'car': 'BMW'},
+    'Gorbachev': {'age': 1984, 'hobby': 'tennis', 'car': 'BMW'},
+    'Kostin': {'age': 2000, 'hobby': 'swimming', 'car': 'Audi'},
+    'Isaev': {'age': 200, 'hobby': 'music', 'car': 'BMW'},
+    'Eliseev': {'age': 1999, 'hobby': 'chess', 'car': 'Audi'},
+    'Kozlov': {'age': 2004, 'hobby': 'soccer', 'car': 'Opel'},
+    'Bukov': {'age': 1995, 'hobby': 'basketball', 'car': 'Audi'},
+    'Gavrilov': {'age': 1980, 'hobby': 'music', 'car': 'Opel'},
+    'Efremov': {'age': 2006, 'hobby': 'tennis', 'car': 'Audi'},
+}
+
+# Заполнение списка ключами:
+b = [elem for elem in a]
+print(b)
+
+b = [{elem, a[elem]['car']} for elem in a if a [elem]['age'] < 2000 and a[elem]['hobby']=='soccer']
+print(b)
