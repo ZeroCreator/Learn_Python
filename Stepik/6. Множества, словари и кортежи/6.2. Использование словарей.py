2578b16d110a741bb9dc2ca70aaf9f22ef5b5a13
ZeroCreator <zerocreator@yahoo.com>
2021-07-30 21:05:56 +0300

    Lesson 6.2

--- Stepik/6. Множества, словари и кортежи/6.2. Использование словарей.py
+++ Stepik/6. Множества, словари и кортежи/6.2. Использование словарей.py
@@ -32,6 +32,50 @@ for i in s.lower():
         nomer = ord(i) - 97
         letters[nomer] += 1
 
-for i in range(26):
-    if letters[i] > 0:
-        print(chr(i + 97), letters[i])
+print(letters) # Разряженный список - много пустых элементов.
+
+#for i in range(26):
+#    if letters[i] > 0:
+#        print(chr(i + 97), letters[i])
+
+# 3. Установить соответствие между обектами.
+print('Словарь')
+# Соответствие между русским словом и английским.
+'''
+words = {}
+while True:
+    s = input()
+    if s in words:
+        print('Слово', s, 'переводится как', words[s])
+    else:
+        print('Введите перевод слова ', s)
+        words[s] = input()
+'''
+# 4. Хранение данных об обекте.
+contacts = {
+    "John Kennedy": {
+        'birthday': "29 may 1917", 'city': 'Brookline',
+        'phone': None, 'children': 3
+    },
+    "Arnold Schwarzenegger": {
+        'birthday': "30 july 1947", 'city': 'Gradec',
+        'phone': '555-555-555', 'children': 5
+    },
+    "Donald John Trump": {
+        'birthday': "14 july 1946", 'city': 'New York',
+        'phone': '777-777-777', 'children': 4
+
+    }
+}
+
+persons = ["Donald John Trump", "Arnold Schwarzenegger", "John Kennedy"]
+for person in persons:
+    print(person, contacts[person]['birthday'])
+
+for person in persons:
+    birthday = contacts[person]['birthday']
+    city = contacts[person]['city']
+    phone = contacts[person]['phone']
+    children = contacts[person]['children']
+    print(person, contacts[person])
+    print(person, children, phone)
