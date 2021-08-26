0cd019bce293cc8aeb1744280d74ba0d0875c5cb
ZeroCreator <ZeroCreator@yahoo.com>
2021-08-26 16:52:52 +0300

    OOП - 2.3

--- stepik - ООП от egoroff_channel/2. Методы и свойства/2.3. Практика создания класса и его методов.py
+++ stepik - ООП от egoroff_channel/2. Методы и свойства/2.3. Практика создания класса и его методов.py
@@ -1,1 +1,11 @@
 # Практика создания класса и его методов.
+# Класс Point.
+class Point:
+
+    def __init__(self, coord_x, coord_y):
+        self.coord_x = coord_x
+        self.coord_y = coord_y
+
+
+p1 = Point(3, 4)
+p2 = Point(-54, 32)
