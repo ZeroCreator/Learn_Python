854d1575ee96132c21cc05ee95bc731958d5e288
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2021-08-31 21:57:54 +0300

    ООП 2.7

--- stepik - ООП от egoroff_channel/2. Методы и свойства/2.7. Декоратор Property.py
+++ stepik - ООП от egoroff_channel/2. Методы и свойства/2.7. Декоратор Property.py
@@ -34,3 +34,27 @@ print(a.my_balance)
 
 a.set_balance = 500
 print(a.name)
+
+'''
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+        
+    @property
+    def my_balance(self):
+        print('get balance')
+        return self.__balance
+        
+    @my_balance.setter
+    def my_balance(self, value):
+        print('set balance')
+        if not isinstance(value,(int, float)):
+            raise ValueError('Баланс должен быть числом')
+        self.__balance = value
+    
+    @my_balance.deleter
+    def my_balance(self):
+        print('delete balance')
+        del self.__balance
+'''
