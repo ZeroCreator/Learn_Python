baabb5ee77e22a5aee516c9a20e5166079b69ffc
ZeroCreator <ZeroCreator@yahoo.com>
2021-11-03 13:42:33 +0300

    Lesson 9.8

--- stepik - Python от Балакирева/9. Генераторы/9.8.  Функция isinstance для проверки типов данных.py
+++ stepik - Python от Балакирева/9. Генераторы/9.8.  Функция isinstance для проверки типов данных.py
@@ -76,3 +76,46 @@ def get_sum(lst):
 # def get_even_sum(it): ...
 # Вызывать функцию не нужно, только определить. Также ничего не нужно выводить на экран.
 # P. S. Не забудьте про необходимость различения булевых значений (False, True) от целочисленных.
+def get_even_sum(it):
+    s = []
+    for i in it:
+        if type(i)==int:
+            s.append(i)
+    return sum(filter(lambda x: x % 2 == 0, s))
+
+#
+def get_even_sum(it):
+    return sum(filter(lambda x: type(x) == int and x % 2 == 0, it))
+
+#
+def get_even_sum(it):
+    return sum(x for x in it if isinstance(x, int) and not isinstance(x, bool) and x % 2 == 0)
+
+
+#
+get_even_sum = lambda a: sum(i for i in a if type(i) is int and i % 2 == 0)
+
+
+# Определите функцию с именем get_list_dig, которая возвращает список только из числовых значений переданной ей
+# коллекции (список или кортеж).
+# Сигнатура функции должна быть, следующей:
+# def get_list_dig(lst): ...
+# Вызывать функцию не нужно, только определить. Также ничего не нужно выводить на экран.
+# P. S. Не забудьте про необходимость различения булевых значений (False, True) от целочисленных.
+def get_list_dig(lst):
+    return list(filter(lambda x: type(x) in (int, float), lst))
+
+print(get_list_dig([4.5, 8.7, True, "книга", 8, 10, -11, [True, False]]))
+
+#
+def get_list_dig(it):
+    return list(filter(lambda x: type(x) in (int, float), it))
+
+#
+def get_list_dig(it):
+    return list(filter(lambda x: isinstance(x, (int, float)) and not isinstance(x, bool), it))
+
+#
+def get_list_dig(lst):
+    return [x for x in lst if type(x) in (int, float, complex)]
+
