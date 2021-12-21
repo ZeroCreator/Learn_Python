2f57baa385f95041deb49c26512fa2c102a04f5a
SHkola_OA <shkola.olga@gmail.com>
2021-12-21 13:25:48 +0300

    TKinter 20

--- Tkinter/game_minesweeper/20-Minesweeper.Подсчитываем мины вокруг.py
+++ Tkinter/game_minesweeper/20-Minesweeper.Подсчитываем мины вокруг.py
@@ -10,8 +10,9 @@ class MyButton(tk.Button):
         super(MyButton, self).__init__(master, width=3, font='Calibri 15 bold', *args, **kwargs)
         self.x = x
         self.y = y
-        self.number = number # нумеруем каждую кнопку для определения количества кнопок
+        self.number = number  # нумеруем каждую кнопку для определения количества кнопок
         self.is_mine = False
+        self.count_bomb = 0  # счетчик мин
 
     def __repr__(self):
         return f"MyButton {self.x} {self.y} {self.number} {self.is_mine}"
@@ -25,9 +26,9 @@ class MineSweeper:
 
     def __init__(self):
         self.buttons = []
-        for i in range(MineSweeper.ROW+2):
+        for i in range(MineSweeper.ROW + 2):
             temp = []  # временный список
-            for j in range(MineSweeper.COLUMN+2):
+            for j in range(MineSweeper.COLUMN + 2):
                 btn = MyButton(MineSweeper.window, x=i, y=j)
                 btn.config(command=lambda button=btn: self.click(button))
                 temp.append(btn)
@@ -39,38 +40,36 @@ class MineSweeper:
             clicked_button.config(text='*', background='red', disabledforeground='black')
         else:
             clicked_button.config(text=clicked_button.number, disabledforeground='black')
-        clicked_button.config(state='disabled') # выключение повторного нажатия кнопки
+        clicked_button.config(state='disabled')  # выключение повторного нажатия кнопки
 
     # отрисовка виджетов
     def create_widgets(self):
-        for i in range(MineSweeper.ROW+2):
-            for j in range(MineSweeper.COLUMN+2):
+        for i in range(1, MineSweeper.ROW + 1):
+            for j in range(1, MineSweeper.COLUMN + 1):
                 btn = self.buttons[i][j]
                 btn.grid(row=i, column=j)
 
-
     def open_all_buttons(self):
-        for i in range(MineSweeper.ROW+2):
-            for j in range(MineSweeper.COLUMN+2):
+        for i in range(MineSweeper.ROW + 2):
+            for j in range(MineSweeper.COLUMN + 2):
                 btn = self.buttons[i][j]
                 if btn.is_mine:
                     btn.config(text='*', background='red', disabledforeground='black')
                 else:
-                    btn.config(text=btn.number, disabledforeground='black')
+                    btn.config(text=btn.count_bomb, disabledforeground='black')
 
     def start(self):
         self.create_widgets()
         self.insert_mines()
+        self.count_mines_in_buttons()
         self.print_buttons()
         self.open_all_buttons()
         print(self.get_mines_places())
         MineSweeper.window.mainloop()
 
-
     def print_buttons(self):
         for row_btn in self.buttons:
             print(row_btn)
-
 
     # метод для вставки мин
     def insert_mines(self):
@@ -85,7 +84,22 @@ class MineSweeper:
                     btn.is_mine = True
                 count += 1
 
-    # метод для расположения бомб
+    # функция подсчета мин
+    def count_mines_in_buttons(self):
+        for i in range(1, MineSweeper.ROW + 1):
+            for j in range(1, MineSweeper.COLUMN + 1):
+                btn = self.buttons[i][j]
+                count_bobm = 0
+                if not btn.is_mine:
+                    for row_dx in [-1, 0, 1]:
+                        for col_dx in [-1, 0, 1]:
+                            neighbour = self.buttons[i + row_dx][
+                                j + col_dx]  # получаем всех возможных соседей для кнопки button
+                            if neighbour.is_mine:
+                                count_bobm += 1
+                btn.count_bomb = count_bobm
+
+    # метод для расположения мин
     @staticmethod
     def get_mines_places():
         indexes = list(range(1, MineSweeper.COLUMN * MineSweeper.ROW + 1))
