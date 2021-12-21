2039899e875c6d57234093ef75f6540db5b33b48
SHkola_OA <shkola.olga@gmail.com>
2021-12-21 14:49:05 +0300

    oop 2.9

--- stepik - ООП от egoroff_channel/2. Методы и свойства/2.9. Classmethod and staticmethod.py
+++ stepik - ООП от egoroff_channel/2. Методы и свойства/2.9. Classmethod and staticmethod.py
@@ -66,3 +66,64 @@ g = a.class_hello()
 print(g)
 
 print(a.__class__)
+
+#
+print("Tasks")
+# Создайте класс Robot, у которого есть:
+# атрибут класса population. В этом атрибуте будет хранится общее количество роботов, изначально принимает значение 0;
+# конструктор __init__, принимающий 1 аргумент name. Данный метод должен сохранять атрибут name и печатать сообщение
+# вида "Робот <name> был создан". Помимо инициализации робота данный метод должен увеличивать популяцию роботов на единицу;
+# метод destroy, должен уменьшать популяцию роботов на единицу и печатать сообщение вида "Робот <name> был уничтожен"
+# метод say_hello, которой печатает сообщение вида "Робот <name> приветствует тебя, особь человеческого рода"
+# метод класса  how_many, который печатает сообщение вида "<population>, вот сколько нас еще осталось"
+# r2 = Robot("R2-D2") # печатает "Робот R2-D2 был создан"
+# r2.say_hello() # печатает "Робот R2-D2 приветствует тебя, особь человеческого рода"
+# Robot.how_many() # печатает "1, вот сколько нас еще осталось"
+# r2.destroy() # печатает "Робот R2-D2 был уничтожен"
+
+class Robot:
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        print(f"Робот {self.name} был создан")
+        Robot.population += 1
+
+    def destroy(self):
+        Robot.population -= 1
+        print(f"Робот {self.name} был уничтожен")
+
+    def say_hello(self):
+        print(f"Робот {self.name} приветствует тебя, особь человеческого рода")
+
+    @classmethod
+    def how_many(cls):
+        print(f"{cls.population}, вот сколько нас еще осталось")
+
+
+r2 = Robot("R2-D2") # печатает "Робот R2-D2 был создан"
+r2.say_hello() # печатает "Робот R2-D2 приветствует тебя, особь человеческого рода"
+Robot.how_many() # печатает "1, вот сколько нас еще осталось"
+r2.destroy() # печатает "Робот R2-D2 был уничтожен"
+
+
+#
+class Robot:
+    population = 0
+
+    def __init__(self, name):
+        self.name=name
+        print(f'Робот {self.name} был создан')
+        Robot.population += 1
+
+
+    def destroy(self):
+        Robot.population -= 1
+        print(f"Робот {self.name} был уничтожен")
+
+    def say_hello(self):
+        print(f'Робот {self.name} приветствует тебя, особь человеческого рода')
+
+    @staticmethod
+    def how_many():
+        print(f'{Robot.population}, вот сколько нас еще осталось')
