0bad51d05b49bea8f54038d15432cdd3195ac375
SHkola_OA <shkola.olga@gmail.com>
2022-02-17 12:24:42 +0300

    Lesson 2.1

---
+++ stepik - Python от Балакирева/2. Переменные, операторы присваивания/2.1. Переменные, операторы присваивания.py
@@ -0,0 +1,23 @@
+# Переменные, оператор присваивания, функции type и id
+# Динамическая типизация
+a = b = c = 0
+print(id(a), id(b), id(c))
+
+a, b = 1, 2
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+# Типы данных
+print(type(a), type(b), type(c))
+x = 5.8
+s = "Hello"
+print(type(x), type(s))
+
+# Ключевые слова
+# help
+# keywords
+print(6)
+
+
