376ccb709c32a3404bd57c941379a8bc6773060c
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-04-07 19:05:22 +0300

    Bubble sort

--- Сортировка/1. Bubble sort.py
+++ Сортировка/1. Bubble sort.py
@@ -14,3 +14,17 @@ def bubble_sort(nums):
 random_list_of_nums = [5, 2, 1, 8, 4]
 bubble_sort(random_list_of_nums)
 print(random_list_of_nums)
+
+
+###
+def bubble_sort(nums: list):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+random_list_of_nums = list(map(int, input().split()))
+bubble_sort(random_list_of_nums)
+print(*random_list_of_nums)
