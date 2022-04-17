d5e5cc61e89ff9b00c9a9aabac9a5f5c24b56daf
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-04-17 20:09:29 +0300

    quick sort

--- Сортировка/6. Quick sort.py
+++ Сортировка/6. Quick sort.py
@@ -1,4 +1,4 @@
-# Быстрая сортировка (Quick sort).
+# Быстрая сортировка (Quick sort).- Сортировка Тони Хоара
 # Есть разные способы реализовать быструю сортировки
 # мы выбрали схема Tony Hoare
 def partition(nums, low, high):
@@ -32,3 +32,30 @@ def quick_sort(nums):
 random_list_of_nums = [22, 5, 1, 18, 99]
 quick_sort(random_list_of_nums)
 print(random_list_of_nums)
+
+
+def hoar_sort(A):
+    if len(A) <= 1:
+        return # returne None
+    barrier = A[0]
+    L = []
+    M = []
+    R = []
+    for x in A:
+        if x < barrier:
+            L.append(x)
+        elif x == barrier:
+            M.append(x)
+        else:
+            R.append(x)
+
+    hoar_sort(L)
+    hoar_sort(R)
+    k = 0
+    for x in L + M + R:
+        A[k] = x
+        k += 1
+
+
+
+
