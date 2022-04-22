22d2c4ac2d55bbf1bcf87b9f48949dd4a6644321
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-04-22 20:43:41 +0300

    Уничтожение объектов: __del__

--- Магические методы/__del__.py
+++ Магические методы/__del__.py
@@ -3,10 +3,12 @@ class Life:
     def __init__(self, name="unknown"):
         print("Hello", name)
         self.name = name
+
     def __del__(self):
         print("Goodbye", self.name)
 
+
 brian = Life("Brian")
-#Hello Brian
+# Hello Brian
 brian = "loretta"
-#Goodbye Brian
+# Goodbye Brian
