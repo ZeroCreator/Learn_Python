a344982bff7daffdd7b9a3975ade52ceb6203e51
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-04-22 16:28:05 +0300

    Магические методы __getitem__, __setitem__ и __delitem__

--- ООП от Балакирева/18. Магические методы __getitem__, __setitem__ и __delitem__.py
+++ ООП от Балакирева/18. Магические методы __getitem__, __setitem__ и __delitem__.py
@@ -1,1 +1,40 @@
 # Магические методы __getitem__, __setitem__ и __delitem__
+# __getitem__(self, item) – получение значения по ключу item;
+# __setitem__(self, key, value) – запись значения value по ключу key;
+# __delitem__(self, key) – удаление элемента по ключу key.
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = list(marks)
+
+    def __getitem__(self, item):
+        if 0 <= item < len(self.marks):
+            return self.marks[item]
+        else:
+            raise IndexError("Неверный индекс")
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, int) or key < 0:
+            raise TypeError("Индекс должен быть целым неотрицательным числом")
+
+        if key >= len(self.marks):
+            off = key + 1 - len(self.marks)
+            self.marks.extend([None] * off)
+
+        self.marks[key] = value
+
+    def __delitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError("Индекс должен быть целым числом")
+
+        del self.marks[key]
+
+s1 = Student('Сергей', [5, 5, 3, 2, 5])
+del s1[2]
+print(s1.marks[2])
+print(s1[2])
+#print(s1[20])
+s1[10] = 4
+print(s1[2])
+print(s1.marks)
