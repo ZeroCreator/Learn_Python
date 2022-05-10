47e1456799d7ebe0b2e28699e92e0faac652d975
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-05-10 17:57:19 +0300

    Шифр Цезаря

--- Шифр Цезаря.py
+++ Шифр Цезаря.py
@@ -11,8 +11,53 @@ def decode(text, step):
         str.maketrans(ALPHA[step:] + ALPHA[:step], ALPHA))
 
 
+def encrypt(text, s):
+    result = ""
+    # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+    # Encrypt uppercase characters in plain text
+        if (char.isupper()):
+            result += chr((ord(char) + s - 65) % 26 + 65)
+    # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
+# check the above function
+text = "CEASER CIPHER DEMO"
+s = 4
+
+
+# взлома алгоритма шифрования Цезаря
+message = 'GIEWIVrGMTLIVrHIQS' #encrypted message
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(LETTERS)):
+   translated = ''
+   for symbol in message:
+      if symbol in LETTERS:
+         num = LETTERS.find(symbol)
+         num = num - key
+         if num < 0:
+            num = num + len(LETTERS)
+         translated = translated + LETTERS[num]
+      else:
+         translated = translated + symbol
+print('Hacking key #%s: %s' % (key, translated))
+
 
 if __name__ == "__main__":
     r = encode("Отче наш", 6)
     print(r)
     print(decode(r, 6))
+    print("Plain Text : " + text)
+    print("Shift pattern : " + str(s))
+    print("Cipher: " + encrypt(text, s))
+
+
+
+
+
+
+
+
